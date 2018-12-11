eb8dbadd707a9565f8ea4e518020e524cf2eabc3
lixilinx <37510137+lixilinx@users.noreply.github.com>
2018-12-10 16:35:05 -0800

    Add files via upload

--- train_svhn.py
+++ train_svhn.py
@@ -6,19 +6,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.autograd import grad
 import torch.nn.functional as F
-import preconditioned_stochastic_gradient_descent as psgd 
+import preconditioned_stochastic_gradient_descent as psgd#download psgd from https://github.com/lixilinx/psgd_torch 
 import utilities as U
 
 batch_size = 32
-lr = 0.01 
-max_num_iter = int(1e5)#1e5 iterations are about 13.6 epochs; totally 235755 samples
-model_name = 'svhn_model'
-enable_dropout = False#our model is small (compared with others for this task), dropout is not very helpful
+lr = 0.01#will aneal learning_rate to 0.001, 0.0001
+num_epochs = 21#about two days on 1080ti with model ks_5_dims_128_192_256 (7.5M coefficients)
+model_name = 'svhn_model_ks_5_dims_128_192_256'
+enable_dropout = False#our model is small (compared with others for this task), dropout may not be very helpful or may need more epochs to train
 train_from_sketch = True
 pre_check_svhn_reading = True
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
  
-# I prepared these mat files with matlab/octave; see readme
+# I prepared these mat files with matlab/octave; you may download them from https://drive.google.com/drive/folders/1BvVpUw3OY3RtkJo-bfujwBsa0y6e49lZ
 train_images, train_labels = U.read_svhn_mat('./svhn/train.mat')
 len_train = len(train_images)
 rp = np.random.permutation(len_train)
@@ -52,15 +52,13 @@ def get_batch():#read training data; do some augumentations
         images = extra2_images[i:i+batch_size]
         labels = extra2_labels[i:i+batch_size]    
         
-    rand_shift1, rand_shift2 = random.randint(0, 4), random.randint(0, 4)
-    images = images[:,:, rand_shift1:rand_shift1+60, rand_shift2:rand_shift2+60]#with shape (B, C, H, W)
+    i, j = random.randint(0, 4), random.randint(0, 4)
+    images = images[:,:, i:i+60, j:j+60]
     images = images[:, np.random.permutation(3)]#randomly permute (R, G, B)
-    #if np.random.randn()>0:#random RGB gains
-    #    images = images*np.random.rand(1,3,1,1)
     old_height, old_width = images.shape[2], images.shape[3]
-    new_size = random.randint(40, 80)#random height rescaling
+    new_size = random.randint(40, 70)#random height rescaling
     images = images[:,:,(np.arange(new_size)*(old_height-1)/(new_size-1)).astype(int)]
-    new_size = random.randint(40, 80)#random width rescaling
+    new_size = random.randint(40, 70)#random width rescaling
     images = images[:,:,:,(np.arange(new_size)*(old_width-1)/(new_size-1)).astype(int)]
     return images, labels
 
@@ -74,7 +72,7 @@ if pre_check_svhn_reading:#do the data make sense?
    
 if train_from_sketch:
     # model coefficients
-    ks, dim1, dim2, dim3 = 5, 48, 64, 96
+    ks, dim1, dim2, dim3 = 5, 128, 192, 256
     dim0 = 3#RGB images
     W1 = torch.tensor(torch.randn(dim0*ks*ks+1, dim1)/(dim0*ks*ks)**0.5, requires_grad=True, device=device)
     W2 = torch.tensor(torch.randn(dim1*ks*ks+1, dim1)/(dim1*ks*ks)**0.5, requires_grad=True, device=device)
@@ -127,38 +125,73 @@ def train_loss(images, labels):
         loss -= U.log_prb_labels(y[i], labels[i])    
     return loss/y.shape[0]/y.shape[2]/y.shape[3]
 
+test_images, test_labels = U.read_svhn_mat('./svhn/test.mat')
+def test_loss():    
+    num_errors = 0
+    with torch.no_grad():
+        for im_cnt, im in enumerate(test_images):
+            image = torch.tensor(im/256, dtype=torch.float, device=device)
+            y = model(image[None])[0]
+
+            transcribed_result = []#the current transcription method is too coarse
+            last_detected_label = -1
+            for j in range(y.shape[2]):
+                hist = np.zeros(10)
+                for i in range(y.shape[1]):
+                    _, label = torch.max(y[:,i,j], dim=0)
+                    if label < 10:                   
+                        hist[label.item()] += 1
+                        
+                if max(hist)>0:# one digit is detected
+                    label = np.argmax(hist)
+                    if label!=last_detected_label:
+                        transcribed_result.append(label)
+                else:#blank
+                    label = -1
+                    
+                last_detected_label = label
+                
+            if transcribed_result!=test_labels[im_cnt]:
+                num_errors += 1
+    return num_errors/len(test_images)
+
 # train our model; use PSGD-Newton for optimization (virtually tuning free)
 Ws = [W1,W2,W3,W4,W5,W6,W7,W8,W9,W10]
-Qs = [[torch.eye(W.shape[0], device=device), torch.eye(W.shape[1], device=device)] for W in Ws]
+Qs = [[torch.eye(W.shape[0], device=device), 0.1*torch.eye(W.shape[1], device=device)] for W in Ws]
 grad_norm_clip_thr = 0.1*sum(W.shape[0]*W.shape[1] for W in Ws)**0.5 
-TrainLoss = []
+TrainLoss, TestLoss, BestTestLoss = [], [], 1e30
 t0 = time.time()
-for num_iter in range(max_num_iter):
-    images, labels = get_batch()
-        
-    new_images = torch.tensor(images/256, dtype=torch.float, device=device)
-    new_labels = []
-    for label in labels:
-        new_labels.append(U.remove_repetitive_labels(label))
-        
-    loss = train_loss(new_images, new_labels)
-    grads = grad(loss, Ws, create_graph=True)
-    TrainLoss.append(loss.item())     
-
-    v = [torch.randn(W.shape, device=device) for W in Ws]
-    Hv = grad(grads, Ws, v)  
-    with torch.no_grad():
-        Qs = [psgd.update_precond_kron(q[0], q[1], dw, dg) for (q, dw, dg) in zip(Qs, v, Hv)]
-        pre_grads = [psgd.precond_grad_kron(q[0], q[1], g) for (q, g) in zip(Qs, grads)]
-        grad_norm = torch.sqrt(sum([torch.sum(g*g) for g in pre_grads]))
-        lr_adjust = min(grad_norm_clip_thr/(grad_norm + 1.2e-38), 1.0)
-        for i in range(len(Ws)):
-            Ws[i] -= lr_adjust*lr*pre_grads[i]
-        
-    if num_iter%100==0:     
-        print('iter: {}; train loss: {}; elapsed time: {}'.format(num_iter, TrainLoss[-1], time.time()-t0))
+for epoch in range(num_epochs):
+    for num_iter in range(int((len_train+len_extra1+len_extra2)/batch_size)):
+        images, labels = get_batch()
+            
+        new_images = torch.tensor(images/256, dtype=torch.float, device=device)
+        new_labels = []
+        for label in labels:
+            new_labels.append(U.remove_repetitive_labels(label))
+            
+        loss = train_loss(new_images, new_labels)
+        grads = grad(loss, Ws, create_graph=True)
+        TrainLoss.append(loss.item())     
+    
+        v = [torch.randn(W.shape, device=device) for W in Ws]
+        Hv = grad(grads, Ws, v)  
+        with torch.no_grad():
+            Qs = [psgd.update_precond_kron(q[0], q[1], dw, dg) for (q, dw, dg) in zip(Qs, v, Hv)]
+            pre_grads = [psgd.precond_grad_kron(q[0], q[1], g) for (q, g) in zip(Qs, grads)]
+            grad_norm = torch.sqrt(sum([torch.sum(g*g) for g in pre_grads]))
+            lr_adjust = min(grad_norm_clip_thr/(grad_norm + 1.2e-38), 1.0)
+            for i in range(len(Ws)):
+                Ws[i] -= lr_adjust*lr*pre_grads[i]
+            
+        if num_iter%100==0:     
+            print('epoch: {}; iter: {}; train loss: {}; elapsed time: {}'.format(epoch, num_iter, TrainLoss[-1], time.time()-t0))
+       
+    TestLoss.append(test_loss())
+    if TestLoss[-1] < BestTestLoss:
+        BestTestLoss = TestLoss[-1]
         with open(model_name, 'wb') as f:
             pickle.dump(Ws, f)
-            
-    if num_iter==int(max_num_iter/2):
+    print('epoch: {}; best test loss: {}; learning rate: {}'.format(epoch, BestTestLoss, lr))
+    if epoch+1==int(num_epochs/3) or epoch+1==int(num_epochs*2/3):
         lr *= 0.1
